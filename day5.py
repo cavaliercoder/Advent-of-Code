d89cbb3bdde0d1c86ac7727bb3be8e442237818c
Ryan Armstrong <ryan@cavaliercoder.com>
2020-10-12 01:45:53 -0700

    Refactor

--- day5.py
+++ day5.py
@@ -3,23 +3,40 @@
 from typing import List
 import unittest
 
-from intcode import decode, Data, IntcodeVM
-
-
-def load_vm(stdin: Data) -> Data:
-    with open("./day5.input", "r") as fp:
-        s = fp.readline()
-    data = decode(s)
-    vm = IntcodeVM(data, stdin)
-    vm.run()
-    return vm.stdout
+from intcode import decode, Data, IntcodeVM, run, load_and_run
 
 
 class TestIntcodeVM(unittest.TestCase):
     def test_part1(self):
-        stdout = load_vm([1])
+        stdout = load_and_run("./day5.input", stdin=[1])
         self.assertEqual(stdout[len(stdout) - 1], 7988899)
-    
+
     def test_part2(self):
-        stdout = load_vm([5])
+        stdout = load_and_run("./day5.input", stdin=[5])
         self.assertEqual(stdout[len(stdout) - 1], 13758663)
+
+    def test_part2_example1(self):
+        data = decode("3,9,8,9,10,9,4,9,99,-1,8")
+        for i in range(10):
+            stdout = run(data, [i])
+            self.assertListEqual(stdout, [1 if i == 8 else 0])
+
+    def test_part2_example2(self):
+        data = decode("3,9,7,9,10,9,4,9,99,-1,8")
+        for i in range(10):
+            stdout = run(data, [i])
+            self.assertListEqual(stdout, [1 if i < 8 else 0])
+
+    def test_part2_example3(self):
+        data = decode("3,3,1108,-1,8,3,4,3,99")
+        for i in range(10):
+            stdout = run(data, [i])
+            self.assertListEqual(
+                stdout, [1 if i == 8 else 0], f"Failed with input: {i}"
+            )
+
+    def test_part2_example4(self):
+        data = decode("3,3,1107,-1,8,3,4,3,99")
+        for i in range(10):
+            stdout = run(data, [i])
+            self.assertListEqual(stdout, [1 if i < 8 else 0])
--- intcode.py
+++ intcode.py
@@ -2,26 +2,51 @@
 
 import logging
 
-from typing import List, Optional, Sequence, Tuple
-
-
-ADDR_MODE_POS = 0
-ADDR_MODE_IMM = 1
-ADDR_MODE_REL = 2
-
-OPCODE_ADD = 1
-OPCODE_MULTIPLY = 2
-OPCODE_INPUT = 3
-OPCODE_OUTPUT = 4
-OPCODE_JCC = 5
-OPCODE_JNC = 6
-OPCODE_LT = 7
-OPCODE_EQ = 8
-OPCODE_RELBASE = 9
-OPCODE_HALT = 99
+from enum import Enum
+from typing import Any, Dict, List, Optional, Sequence, Tuple
 
 
 Data = List[int]
+
+Address = int
+
+
+class AddressMode(Enum):
+    POSITION = 0
+    IMMEDIATE = 1
+    RELATIVE = 2
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Opcode(Enum):
+    UNKNOWN = 0
+    ADD = 1
+    MULTIPLY = 2
+    INPUT = 3
+    OUTPUT = 4
+    JCC = 5
+    JNC = 6
+    LT = 7
+    EQ = 8
+    RELBASE = 9
+    HALT = 99
+
+
+OPCODE_SIGNATURES: Dict[Opcode, Tuple[str, List[str]]] = {
+    Opcode.UNKNOWN: ("unknown", []),
+    Opcode.ADD: ("add", ["a", "b", "addr"]),
+    Opcode.MULTIPLY: ("multiply", ["a", "b", "addr"]),
+    Opcode.INPUT: ("input", ["addr"]),
+    Opcode.OUTPUT: ("output", ["addr"]),
+    Opcode.JCC: ("jump-if-true", ["v", "addr"]),
+    Opcode.JNC: ("jump-if-not-true", ["v", "addr"]),
+    Opcode.LT: ("less-than", ["a", "b", "addr"]),
+    Opcode.EQ: ("equal", ["a", "b", "addr"]),
+    Opcode.RELBASE: ("adjust-rel-base", ["offset"]),
+    Opcode.HALT: ("halt", []),
+}
 
 
 logger = logging.getLogger(__name__)
@@ -31,62 +56,104 @@ def decode(s: str) -> Data:
     return [int(opcode) for opcode in s.split(",")]
 
 
-class Instruction:
-    def __init__(self, v: int) -> None:
-        self.opcode: int = v % 100
-        self.params: Sequence[int] = []
-
-        v = int(v / 100)
-        while v:
-            self.params.append(v % 10)
-            v = int(v / 10)
-
-    def get_param(self, n: int) -> int:
-        if n >= len(self.params):
-            return 0
-        return self.params[n]
-
-
 class IntcodeVM:
     def __init__(
         self, data: Optional[Data] = None, stdin: Optional[Data] = None
     ) -> None:
         self.handlers = {
-            OPCODE_ADD: self.op_add,
-            OPCODE_MULTIPLY: self.op_multiply,
-            OPCODE_INPUT: self.op_input,
-            OPCODE_OUTPUT: self.op_output,
-            OPCODE_JCC: self.op_jcc,
-            OPCODE_JNC: self.op_jnc,
-            OPCODE_LT: self.op_lt,
-            OPCODE_EQ: self.op_eq,
-            OPCODE_RELBASE: self.op_relbase,
+            Opcode.ADD: self._op_add,
+            Opcode.MULTIPLY: self._op_multiply,
+            Opcode.INPUT: self._op_input,
+            Opcode.OUTPUT: self._op_output,
+            Opcode.JCC: self._op_jump_if_true,
+            Opcode.JNC: self._op_jump_if_not_true,
+            Opcode.LT: self._op_less_than,
+            Opcode.EQ: self._op_equal,
+            Opcode.RELBASE: self._op_relbase,
         }
         self.reset(data, stdin)
 
+    def reset(self, data: Optional[Data], stdin: Optional[Data] = None) -> None:
+        # program counter
+        self.reg_pc = Address(0)
+
+        # dedicated register for the relative offset base
+        self.reg_rel_base = Address(0)
+
+        # vm memory
+        self.data = data.copy() if data else []
+
+        # input buffer
+        self.stdin: Data = stdin.copy() if stdin else []
+
+        # output buffer
+        self.stdout: Data = []
+
+        # current instruction opcode
+        self.opcode: Opcode = Opcode.UNKNOWN
+
+        # current instruction parameter address modes
+        self.param_modes: List[AddressMode] = [AddressMode.POSITION] * 4
+
+    def _trace(
+        self,
+    ) -> None:
+        sig = OPCODE_SIGNATURES.get(self.opcode)
+        if sig is None:
+            sig = OPCODE_SIGNATURES[Opcode.UNKNOWN]
+        opcode = sig[0]
+        params = sig[1]
+
+        def encode_param(i: int) -> str:
+            addr = self.reg_pc + 1 + i
+            mode = self.param_modes[i]
+            name = params[i]
+            v = self.data[addr]
+            if mode == AddressMode.POSITION:
+                return f"{name}={v}"
+            if mode == AddressMode.IMMEDIATE:
+                return f"{name}=#{v}"
+            if mode == AddressMode.RELATIVE:
+                return f"{name}={v}+${self.reg_rel_base}"
+            raise RuntimeError(f"Unrecognized address mode: {mode}")
+
+        params_str = ", ".join([encode_param(i) for i in range(len(params))])
+        raw_str = ",".join(
+            str(i) for i in self.data[self.reg_pc : self.reg_pc + len(params) + 1]
+        )
+        logger.debug(f"[{self.reg_pc:04}] {opcode}({params_str})  # {raw_str}")
+
+    def _log_result(self, result: Any) -> None:
+        logger.debug(f"  -> {result}")
+
     @property
     def halted(self) -> bool:
-        fn = Instruction(self.data[self.ptr])
-        return fn.opcode == OPCODE_HALT
-
-    def reset(self, data: Optional[Data], stdin: Optional[Data] = None) -> None:
-        self.ptr = 0
-        self.reg_rel_base = 0
-        self.data = data.copy() if data else []
-        self.stdin: Data = stdin.copy() if stdin else []
-        self.stdout: Data = []
+        return self.opcode == Opcode.HALT
 
     def step(self) -> bool:
         """
         Fetch, decode and execute a single instruction.
         """
-        fn = Instruction(self.data[self.ptr])
-        if fn.opcode == OPCODE_HALT:
-            logger.debug(f"[{self.ptr:04}] HALT")
+        if self.halted:
+            raise RuntimeError("Halted")
+
+        # fetch instruction
+        v = self.read(self.reg_pc)
+
+        # decode opcode and parameter address modes
+        self.opcode = Opcode(v % 100)
+        v = int(v / 100)
+        for i in range(len(self.param_modes)):
+            self.param_modes[i] = AddressMode(v % 10)
+            v = int(v / 10)
+        self._trace()
+
+        # dispatch
+        if self.opcode == Opcode.HALT:
             return False
-        if fn.opcode not in self.handlers:
-            raise RuntimeError(f"Unrecognized opcode: {fn.opcode}")
-        self.handlers[fn.opcode](fn)
+        if self.opcode not in self.handlers:
+            raise RuntimeError(f"Unrecognized opcode: {self.opcode}")
+        self.handlers[self.opcode]()
         return True
 
     def run(self) -> None:
@@ -110,12 +177,12 @@ class IntcodeVM:
         self.stdout = self.stdout[1:]
         return v
 
-    def decode_addr(self, addr: int, mode: int) -> int:
-        if mode == ADDR_MODE_IMM:
+    def effective_addr(self, addr: int, mode: AddressMode) -> int:
+        if mode == AddressMode.IMMEDIATE:
             return addr
-        if mode == ADDR_MODE_POS:
+        if mode == AddressMode.POSITION:
             return self.data[addr]
-        if mode == ADDR_MODE_REL:
+        if mode == AddressMode.RELATIVE:
             return self.data[addr] + self.reg_rel_base
         raise RuntimeError(f"Unrecognized address mode: {mode}")
 
@@ -126,129 +193,141 @@ class IntcodeVM:
         new_size = 64
         while new_size < addr:
             new_size *= 2
+        if new_size > 4096:
+            raise RuntimeError("Too much memory allocated")
         new_data = [0] * new_size
         for i in range(old_size):
             new_data[i] = self.data[i]
         self.data = new_data
+        logger.debug(f"  grow_mem(addr={addr}) -> {new_size}")
         return new_size
 
-    def read(self, addr: int, mode=ADDR_MODE_IMM) -> int:
-        addr = self.decode_addr(addr, mode)
+    def read(self, addr: int, mode=AddressMode.IMMEDIATE) -> int:
+        if addr != AddressMode.IMMEDIATE:
+            addr = self.effective_addr(addr, mode)
+        assert addr >= 0, f"Invalid address: {addr}"
         if addr >= len(self.data):
-            return 0
-        return self.data[addr]
+            v = 0
+        else:
+            v = self.data[addr]
+        logger.debug(f"  read(addr={addr}) -> {v}")
+        return v
 
-    def write(self, addr: int, v: int, mode=ADDR_MODE_POS) -> None:
-        # addr = self.decode_addr(addr, mode)
+    def write(self, addr: int, v: int) -> None:
         self.grow_mem(addr)
         self.data[addr] = v
+        logger.debug(f"  write(addr={addr}, v={v})")
 
-    def op_add(self, fn: Instruction) -> None:
+    def _op_add(self) -> None:
         """
         Opcode 1 adds together numbers read from two positions and stores the result in a third
         position. The three integers immediately after the opcode tell you these three positions
          - the first two indicate the positions from which you should read the input values, and the
         third indicates the position at which the output should be stored.
         """
-        a = self.read(self.ptr + 1, fn.get_param(0))
-        b = self.read(self.ptr + 2, fn.get_param(1))
-        addr = self.decode_addr(self.ptr + 3, fn.get_param(2))
+        a = self.read(self.reg_pc + 1, self.param_modes[0])
+        b = self.read(self.reg_pc + 2, self.param_modes[1])
+        addr = self.effective_addr(self.reg_pc + 3, self.param_modes[2])
         v = a + b
-        self.write(addr, v, fn.get_param(2))
-        logger.debug(f"[{self.ptr:04}] ADD({a}, {b}) -> {v}")
-        self.ptr += 4
+        self.write(addr, v)
+        self.reg_pc += 4
 
-    def op_multiply(self, fn: Instruction) -> None:
+    def _op_multiply(self) -> None:
         """
         Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding
         them. Again, the three integers after the opcode indicate where the inputs and outputs are,
         not their values.
         """
-        a = self.read(self.ptr + 1, fn.get_param(0))
-        b = self.read(self.ptr + 2, fn.get_param(1))
-        addr = self.decode_addr(self.ptr + 3, fn.get_param(2))
+        a = self.read(self.reg_pc + 1, self.param_modes[0])
+        b = self.read(self.reg_pc + 2, self.param_modes[1])
+        addr = self.effective_addr(self.reg_pc + 3, self.param_modes[2])
         v = a * b
-        self.write(addr, v,  fn.get_param(2))
-        logger.debug(f"[{self.ptr:04}] MUL({a}, {b}) -> {v})")
-        self.ptr += 4
+        self.write(addr, v)
+        self.reg_pc += 4
 
-    def op_input(self, fn: Instruction) -> None:
+    def _op_input(self) -> None:
         if self.stdin:
             v = self.stdin[0]
             self.stdin = self.stdin[1:]
         else:
-            v = int(input(f"[{self.ptr:04}] Enter input: "))
-        addr = self.decode_addr(self.ptr + 1, fn.get_param(0))
+            v = int(input(f"[{self.reg_pc:04}] Enter input: "))
+        addr = self.effective_addr(self.reg_pc + 1, self.param_modes[0])
         self.write(addr, v)
-        logger.debug(f"[{self.ptr:04}] INPUT({addr}) -> {v}")
-        self.ptr += 2
+        self.reg_pc += 2
 
-    def op_output(self, fn: Instruction) -> None:
-        v = self.read(self.ptr + 1, fn.get_param(0))
+    def _op_output(self) -> None:
+        v = self.read(self.reg_pc + 1, self.param_modes[0])
         self.stdout.append(v)
-        logger.debug(f"[{self.ptr:04}] OUTPUT() -> {v}")
-        self.ptr += 2
+        self.reg_pc += 2
 
-    def op_jcc(self, fn: Instruction) -> None:
+    def _op_jump_if_true(self) -> None:
         """
         Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction
         pointer to the value from the second parameter. Otherwise, it does nothing.
         """
-        a = self.read(self.ptr + 1, fn.get_param(0))
-        b = self.read(self.ptr + 2, fn.get_param(1))
-        result = a != 0
-        logger.debug(f"[{self.ptr:04}] JCC({a}, {b}) -> {result}")
-        if result:
-            self.ptr = b
+        a = self.read(self.reg_pc + 1, self.param_modes[0])
+        addr = self.read(self.reg_pc + 2, self.param_modes[1])
+        v = a != 0
+        if v:
+            self._log_result(addr)
+            self.reg_pc = addr
         else:
-            self.ptr += 3
+            self.reg_pc += 3
 
-    def op_jnc(self, fn: Instruction) -> None:
+    def _op_jump_if_not_true(self) -> None:
         """
         Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer
         to the value from the second parameter. Otherwise, it does nothing.
         """
-        a = self.read(self.ptr + 1, fn.get_param(0))
-        b = self.read(self.ptr + 2, fn.get_param(1))
-        result = a == 0
-        logger.debug(f"[{self.ptr:04}] JNC({a}, {b}) -> {result}")
-        if result:
-            self.ptr = b
+        a = self.read(self.reg_pc + 1, self.param_modes[0])
+        addr = self.read(self.reg_pc + 2, self.param_modes[1])
+        v = a == 0
+        if v:
+            self.reg_pc = addr
         else:
-            self.ptr += 3
+            self.reg_pc += 3
 
-    def op_lt(self, fn: Instruction) -> None:
+    def _op_less_than(self) -> None:
         """
         Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1
         in the position given by the third parameter. Otherwise, it stores 0.
         """
-        a = self.read(self.ptr + 1, fn.get_param(0))
-        b = self.read(self.ptr + 2, fn.get_param(1))
-        addr = self.decode_addr(self.ptr + 3, fn.get_param(2))
+        a = self.read(self.reg_pc + 1, self.param_modes[0])
+        b = self.read(self.reg_pc + 2, self.param_modes[1])
+        addr = self.effective_addr(self.reg_pc + 3, self.param_modes[2])
         v = int(a < b)
         self.write(addr, v)
-        logger.debug(f"[{self.ptr:04}] LT({a}, {b}, {addr}) -> {v}")
-        self.ptr += 4
+        self.reg_pc += 4
 
-    def op_eq(self, fn: Instruction) -> None:
+    def _op_equal(self) -> None:
         """
         Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in
         the position given by the third parameter. Otherwise, it stores 0.
         """
-        a = self.read(self.ptr + 1, fn.get_param(0))
-        b = self.read(self.ptr + 2, fn.get_param(1))
-        addr = self.decode_addr(self.ptr + 3, fn.get_param(2))
+        a = self.read(self.reg_pc + 1, self.param_modes[0])
+        b = self.read(self.reg_pc + 2, self.param_modes[1])
+        addr = self.effective_addr(self.reg_pc + 3, self.param_modes[2])
         v = int(a == b)
         self.write(addr, v)
-        logger.debug(f"[{self.ptr:04}] EQ({a}, {b}, {addr}) -> {v}")
-        self.ptr += 4
+        self.reg_pc += 4
 
-    def op_relbase(self, fn: Instruction) -> None:
+    def _op_relbase(self) -> None:
         """
         Opcode 9 adjusts the relative base by the value of its only parameter. The relative base
         increases (or decreases, if the value is negative) by the value of the parameter.
         """
-        a = self.read(self.ptr + 1, fn.get_param(0))
+        a = self.read(self.reg_pc + 1, self.param_modes[0])
         self.reg_rel_base += a
-        logger.debug(f"[{self.ptr:04}] RELBASE({a}) -> {self.reg_rel_base}")
-        self.ptr += 2
+        self.reg_pc += 2
+
+
+def run(data: Data, stdin: Optional[Data] = None) -> Data:
+    vm = IntcodeVM(data=data, stdin=stdin)
+    vm.run()
+    return vm.stdout
+
+
+def load_and_run(file: str, stdin: Optional[Data] = None) -> Data:
+    with open(file, "r") as fp:
+        data = decode(fp.readline())
+    return run(data, stdin=stdin)
