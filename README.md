c1ae4a9bd167fe84949d09f67722c3664aa0d398
Ryan Armstrong <ryan@cavaliercoder.com>
2020-10-11 22:55:27 -0700

    Readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Advent of Code 2019
+
+Trying to learn Python in greater depth this year. These are my solutions for
+[Advent of Code 2019](https://adventofcode.com/2019).
+
+Ryan.
--- day9.py
+++ day9.py
@@ -5,36 +5,19 @@ from intcode import IntcodeVM, Data, decode
 
 class TestDay9(unittest.TestCase):
     def test_part1_example1(self):
-        data = [
-            109,
-            1,
-            204,
-            -1,
-            1001,
-            100,
-            1,
-            100,
-            1008,
-            100,
-            16,
-            101,
-            1006,
-            101,
-            0,
-            99,
-        ]
+        data = decode("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99")
         vm = IntcodeVM(data)
         vm.run()
         self.assertListEqual(vm.stdout, data)
 
     def test_part1_example2(self):
-        data = [1102, 34915192, 34915192, 7, 4, 7, 99, 0]
+        data = decode("1102,34915192,34915192,7,4,7,99,0")
         vm = IntcodeVM(data)
         vm.run()
         self.assertGreaterEqual(vm.stdout[0], 1000000000000000)
 
     def test_part1_example3(self):
-        data = [104, 1125899906842624, 99]
+        data = decode("104,1125899906842624,99")
         vm = IntcodeVM(data)
         vm.run()
         self.assertEqual(vm.stdout[0], 1125899906842624)
