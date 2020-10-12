54a45d77664b7c7d9bb1aa4b9f169edc92abc306
Ryan Armstrong <ryan@cavaliercoder.com>
2020-10-11 22:46:58 -0700

    Day 9

---
+++ day9.py
@@ -0,0 +1,54 @@
+import unittest
+
+from intcode import IntcodeVM, Data, decode
+
+
+class TestDay9(unittest.TestCase):
+    def test_part1_example1(self):
+        data = [
+            109,
+            1,
+            204,
+            -1,
+            1001,
+            100,
+            1,
+            100,
+            1008,
+            100,
+            16,
+            101,
+            1006,
+            101,
+            0,
+            99,
+        ]
+        vm = IntcodeVM(data)
+        vm.run()
+        self.assertListEqual(vm.stdout, data)
+
+    def test_part1_example2(self):
+        data = [1102, 34915192, 34915192, 7, 4, 7, 99, 0]
+        vm = IntcodeVM(data)
+        vm.run()
+        self.assertGreaterEqual(vm.stdout[0], 1000000000000000)
+
+    def test_part1_example3(self):
+        data = [104, 1125899906842624, 99]
+        vm = IntcodeVM(data)
+        vm.run()
+        self.assertEqual(vm.stdout[0], 1125899906842624)
+
+    def test_part1(self):
+        with open("./day9.input", "r") as fp:
+            data = decode(fp.readline())
+        vm = IntcodeVM(data, [1])
+        vm.run()
+        self.assertListEqual(vm.stdout, [3335138414])
+
+    def test_part2(self):
+        with open("./day9.input", "r") as fp:
+            data = decode(fp.readline())
+        vm = IntcodeVM(data, [2])
+        vm.run()
+        self.assertListEqual(vm.stdout, [49122])
--- intcode.py
+++ intcode.py
@@ -7,6 +7,7 @@ from typing import List, Optional, Sequence, Tuple
 
 ADDR_MODE_POS = 0
 ADDR_MODE_IMM = 1
+ADDR_MODE_REL = 2
 
 OPCODE_ADD = 1
 OPCODE_MULTIPLY = 2
@@ -16,6 +17,7 @@ OPCODE_JCC = 5
 OPCODE_JNC = 6
 OPCODE_LT = 7
 OPCODE_EQ = 8
+OPCODE_RELBASE = 9
 OPCODE_HALT = 99
 
 
@@ -46,7 +48,9 @@ class Instruction:
 
 
 class IntcodeVM:
-    def __init__(self, data: Optional[Data] = None, stdin: Optional[Data] = None) -> None:
+    def __init__(
+        self, data: Optional[Data] = None, stdin: Optional[Data] = None
+    ) -> None:
         self.handlers = {
             OPCODE_ADD: self.op_add,
             OPCODE_MULTIPLY: self.op_multiply,
@@ -56,9 +60,10 @@ class IntcodeVM:
             OPCODE_JNC: self.op_jnc,
             OPCODE_LT: self.op_lt,
             OPCODE_EQ: self.op_eq,
+            OPCODE_RELBASE: self.op_relbase,
         }
         self.reset(data, stdin)
-    
+
     @property
     def halted(self) -> bool:
         fn = Instruction(self.data[self.ptr])
@@ -66,6 +71,7 @@ class IntcodeVM:
 
     def reset(self, data: Optional[Data], stdin: Optional[Data] = None) -> None:
         self.ptr = 0
+        self.reg_rel_base = 0
         self.data = data.copy() if data else []
         self.stdin: Data = stdin.copy() if stdin else []
         self.stdout: Data = []
@@ -95,7 +101,7 @@ class IntcodeVM:
         Push a new value to the VM's input buffer.
         """
         self.stdin.append(v)
-    
+
     def io_pop(self) -> int:
         """
         Pop a value from the VM's output buffer.
@@ -104,39 +110,77 @@ class IntcodeVM:
         self.stdout = self.stdout[1:]
         return v
 
-    def read(self, addr: int, mode=ADDR_MODE_IMM) -> int:
-        v = self.data[addr]
+    def decode_addr(self, addr: int, mode: int) -> int:
         if mode == ADDR_MODE_IMM:
-            return v
+            return addr
         if mode == ADDR_MODE_POS:
-            return self.data[v]
+            return self.data[addr]
+        if mode == ADDR_MODE_REL:
+            return self.data[addr] + self.reg_rel_base
         raise RuntimeError(f"Unrecognized address mode: {mode}")
 
+    def grow_mem(self, addr: int) -> int:
+        old_size = len(self.data)
+        if addr < old_size:
+            return old_size
+        new_size = 64
+        while new_size < addr:
+            new_size *= 2
+        new_data = [0] * new_size
+        for i in range(old_size):
+            new_data[i] = self.data[i]
+        self.data = new_data
+        return new_size
+
+    def read(self, addr: int, mode=ADDR_MODE_IMM) -> int:
+        addr = self.decode_addr(addr, mode)
+        if addr >= len(self.data):
+            return 0
+        return self.data[addr]
+
+    def write(self, addr: int, v: int, mode=ADDR_MODE_POS) -> None:
+        # addr = self.decode_addr(addr, mode)
+        self.grow_mem(addr)
+        self.data[addr] = v
+
     def op_add(self, fn: Instruction) -> None:
+        """
+        Opcode 1 adds together numbers read from two positions and stores the result in a third
+        position. The three integers immediately after the opcode tell you these three positions
+         - the first two indicate the positions from which you should read the input values, and the
+        third indicates the position at which the output should be stored.
+        """
         a = self.read(self.ptr + 1, fn.get_param(0))
         b = self.read(self.ptr + 2, fn.get_param(1))
-        c = self.read(self.ptr + 3)
-        self.data[c] = a + b
-        logger.debug(f"[{self.ptr:04}] ADD({a}, {b}) -> {self.data[c]} (@ {c})")
+        addr = self.decode_addr(self.ptr + 3, fn.get_param(2))
+        v = a + b
+        self.write(addr, v, fn.get_param(2))
+        logger.debug(f"[{self.ptr:04}] ADD({a}, {b}) -> {v}")
         self.ptr += 4
 
     def op_multiply(self, fn: Instruction) -> None:
+        """
+        Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding
+        them. Again, the three integers after the opcode indicate where the inputs and outputs are,
+        not their values.
+        """
         a = self.read(self.ptr + 1, fn.get_param(0))
         b = self.read(self.ptr + 2, fn.get_param(1))
-        c = self.read(self.ptr + 3)
-        self.data[c] = a * b
-        logger.debug(f"[{self.ptr:04}] MUL({a}, {b}) -> {self.data[c]} (@ {c})")
+        addr = self.decode_addr(self.ptr + 3, fn.get_param(2))
+        v = a * b
+        self.write(addr, v,  fn.get_param(2))
+        logger.debug(f"[{self.ptr:04}] MUL({a}, {b}) -> {v})")
         self.ptr += 4
 
     def op_input(self, fn: Instruction) -> None:
-        addr = self.read(self.ptr + 1)
         if self.stdin:
             v = self.stdin[0]
             self.stdin = self.stdin[1:]
         else:
             v = int(input(f"[{self.ptr:04}] Enter input: "))
-        self.data[addr] = v
-        logger.debug(f"[{self.ptr:04}] INPUT({addr}) -> {self.data[addr]}")
+        addr = self.decode_addr(self.ptr + 1, fn.get_param(0))
+        self.write(addr, v)
+        logger.debug(f"[{self.ptr:04}] INPUT({addr}) -> {v}")
         self.ptr += 2
 
     def op_output(self, fn: Instruction) -> None:
@@ -180,9 +224,10 @@ class IntcodeVM:
         """
         a = self.read(self.ptr + 1, fn.get_param(0))
         b = self.read(self.ptr + 2, fn.get_param(1))
-        c = self.read(self.ptr + 3)
-        self.data[c] = int(a < b)
-        logger.debug(f"[{self.ptr:04}] LT({a}, {b}, {c}) -> {self.data[c]}")
+        addr = self.decode_addr(self.ptr + 3, fn.get_param(2))
+        v = int(a < b)
+        self.write(addr, v)
+        logger.debug(f"[{self.ptr:04}] LT({a}, {b}, {addr}) -> {v}")
         self.ptr += 4
 
     def op_eq(self, fn: Instruction) -> None:
@@ -192,7 +237,18 @@ class IntcodeVM:
         """
         a = self.read(self.ptr + 1, fn.get_param(0))
         b = self.read(self.ptr + 2, fn.get_param(1))
-        c = self.read(self.ptr + 3)
-        self.data[c] = int(a == b)
-        logger.debug(f"[{self.ptr:04}] EQ({a}, {b}, {c}) -> {self.data[c]}")
+        addr = self.decode_addr(self.ptr + 3, fn.get_param(2))
+        v = int(a == b)
+        self.write(addr, v)
+        logger.debug(f"[{self.ptr:04}] EQ({a}, {b}, {addr}) -> {v}")
         self.ptr += 4
+
+    def op_relbase(self, fn: Instruction) -> None:
+        """
+        Opcode 9 adjusts the relative base by the value of its only parameter. The relative base
+        increases (or decreases, if the value is negative) by the value of the parameter.
+        """
+        a = self.read(self.ptr + 1, fn.get_param(0))
+        self.reg_rel_base += a
+        logger.debug(f"[{self.ptr:04}] RELBASE({a}) -> {self.reg_rel_base}")
+        self.ptr += 2
