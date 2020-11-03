6fa40b4c51f74fcb28f9918f58d52b5a2a5c4bd0
Ryan Armstrong <ryan@cavaliercoder.com>
2020-11-02 23:42:52 -0800

    Day 22 Part2

--- go/2019/day22/day22.go
+++ go/2019/day22/day22.go
@@ -7,7 +7,7 @@ import (
 	"io"
 )
 
-type Card uint64
+type Card int64
 
 type Deck struct {
 	cards []Card
@@ -46,6 +46,10 @@ func (d *Deck) IndexOf(card Card) int {
 		}
 	}
 	return -1
+}
+
+func (d *Deck) Get(i int) Card {
+	return d.cards[i]
 }
 
 func (d *Deck) DealIntoNewStack() {
@@ -100,4 +104,95 @@ func (d *Deck) Run(r io.Reader) {
 
 		panic(s)
 	}
+}
+
+type SuperDeck struct {
+	size       int64
+	trackIndex int64
+}
+
+func NewSuperDeck(size int64, trackIndex int64) *SuperDeck {
+	return &SuperDeck{
+		size:       size,
+		trackIndex: trackIndex,
+	}
+}
+
+func (d *SuperDeck) Index() int64 {
+	return d.trackIndex
+}
+
+func (d *SuperDeck) UndoShuffle(shuffles ...SuperDeckShuffle) int64 {
+	for i := 0; i < len(shuffles); i++ {
+		shuffles[len(shuffles)-1-i].Do(d)
+	}
+	return d.Index()
+}
+
+type SuperDeckShuffle interface {
+	Do(d *SuperDeck)
+}
+
+func ReadSuperDeckShuffles(r io.Reader) []SuperDeckShuffle {
+	shuffles := make([]SuperDeckShuffle, 0, 64)
+	var v, n int
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		s := scanner.Text()
+
+		// deal into new stack
+		if s == "deal into new stack" {
+			shuffles = append(shuffles, UndoDealIntoNewDeck())
+			continue
+		}
+
+		// cut
+		if n, _ = fmt.Sscanf(s, "cut %d", &v); n == 1 {
+			shuffles = append(shuffles, UndoCut(int64(v)))
+			continue
+		}
+
+		// deal with increment
+		if n, _ = fmt.Sscanf(s, "deal with increment %d", &v); n == 1 {
+			shuffles = append(shuffles, UndoDealWithIncrement(int64(v)))
+			continue
+		}
+
+		panic(s)
+	}
+	return shuffles
+}
+
+type SuperDeckShuffleFunc func(*SuperDeck)
+
+func (f SuperDeckShuffleFunc) Do(d *SuperDeck) {
+	f(d)
+}
+
+func UndoDealIntoNewDeck() SuperDeckShuffle {
+	return SuperDeckShuffleFunc(func(d *SuperDeck) {
+		d.trackIndex = d.size - 1 - d.trackIndex
+	})
+}
+
+func UndoCut(n int64) SuperDeckShuffle {
+	return SuperDeckShuffleFunc(func(d *SuperDeck) {
+		if n < 0 {
+			n = d.size + n
+		}
+		d.trackIndex = (n + d.trackIndex) % d.size
+	})
+}
+
+func UndoDealWithIncrement(n int64) SuperDeckShuffle {
+	return SuperDeckShuffleFunc(func(d *SuperDeck) {
+		A := make([]int64, n)
+		var v, j int64
+		for i := int64(1); i < n; i++ {
+			v += (d.size-j)/n + 1
+			j = (v * n) % d.size
+			A[j] = v
+		}
+		d.trackIndex = A[d.trackIndex%n] + d.trackIndex/n
+	})
 }
--- go/2019/day22/day22_test.go
+++ go/2019/day22/day22_test.go
@@ -1,6 +1,7 @@
 package day22
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -66,4 +67,101 @@ func TestPart1(t *testing.T) {
 	d := NewDeck(10007)
 	d.Run(f)
 	AssertInt(t, 7860, d.IndexOf(2019), "wrong index of card 2019")
+}
+
+func TestUndoDealIntoNewDeck(t *testing.T) {
+	n := 1024
+	f := UndoDealIntoNewDeck()
+	for i := 0; i < n; i++ {
+		d := NewDeck(n)
+		d.DealIntoNewStack()
+		sd := NewSuperDeck(int64(n), int64(i))
+		f.Do(sd)
+		AssertInt(t, int(d.Get(i)), int(sd.Index()), "bad rewind at index %d", i)
+	}
+}
+
+func TestUndoCut(t *testing.T) {
+	n := 1024
+	for cut := -n + 1; cut < n; cut++ { // TODO: negatives!
+		d := NewDeck(n)
+		d.Cut(cut)
+		for i := 0; i < n; i++ {
+			sd := NewSuperDeck(int64(n), int64(i))
+			UndoCut(int64(cut)).Do(sd)
+			AssertInt(t, int(d.Get(i)), int(sd.Index()), "bad UndoCut(%d) @ %d", cut, i)
+		}
+	}
+}
+
+func TestUndoDealWithIncrement(t *testing.T) {
+	n := 271 // must be prime
+	for incr := 2; incr < n; incr++ {
+		t.Run(fmt.Sprintf("WithIncrement%000d", incr), func(t *testing.T) {
+			d := NewDeck(n)
+			d.DealWithIncrement(incr)
+			for i := 0; i < n; i++ {
+				t.Run(fmt.Sprintf("TrackingIndex%00d", i), func(t *testing.T) {
+					sd := NewSuperDeck(int64(n), int64(i))
+					UndoDealWithIncrement(int64(incr)).Do(sd)
+					AssertInt(t, int(d.Get(i)), int(sd.Index()), "bad card in %v", d)
+				})
+			}
+		})
+	}
+}
+
+func TestUndoShuffle(t *testing.T) {
+	n := 10007
+
+	// run standard deck shuffle once
+	d := NewDeck(10007)
+	func() {
+		f, err := OpenFixture("day22")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer f.Close()
+		d.Run(f)
+	}()
+
+	// load super shuffles
+	var shuffles []SuperDeckShuffle
+	func() {
+		f, err := OpenFixture("day22")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer f.Close()
+		shuffles = ReadSuperDeckShuffles(f)
+	}()
+
+	// test result for each i in n
+	for i := 0; i < n; i++ {
+		t.Run(fmt.Sprintf("WithIndex%0000d", i), func(t *testing.T) {
+			sd := NewSuperDeck(int64(n), int64(i))
+			v := sd.UndoShuffle(shuffles...)
+			AssertInt(t, int(d.Get(i)), int(v), "bad card in %v", d)
+		})
+	}
+}
+
+func TestFindLoops(t *testing.T) {
+	// load super shuffles
+	var shuffles []SuperDeckShuffle
+	func() {
+		f, err := OpenFixture("day22")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer f.Close()
+		shuffles = ReadSuperDeckShuffles(f)
+	}()
+
+	var v int64
+	sd := NewSuperDeck(int64(119315717514047), int64(2020))
+	for i := int64(0); i < 101741582076661; i++ {
+		v = sd.UndoShuffle(shuffles...)
+	}
+	t.Fatal(v)
 }
