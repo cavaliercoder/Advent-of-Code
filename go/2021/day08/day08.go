fc637bc8271748e9f263bf9d9033dfa60f664068
Ryan Armstrong <ryan@cavaliercoder.com>
2021-12-16 23:17:24 +0800

    2021 Day 8

--- go/2021/day08/day08.go
+++ go/2021/day08/day08.go
@@ -1,2 +1,78 @@
 package day08
 
+import "fmt"
+
+type Display byte
+
+func ParseDisplay(b []byte) (Display, error) {
+	var v Display
+	for _, c := range b {
+		c = c - 'a'
+		if c >= 7 {
+			return 0, fmt.Errorf("bad display: %s", b)
+		}
+		v |= 1 << c
+	}
+	return v, nil
+}
+
+func (d Display) SegmentCount() (n int) {
+	for d != 0 {
+		n++
+		d &= d - 1
+	}
+	return
+}
+
+func Rewire(a [10]Display) [10]Display {
+	var known [10]Display
+	for i := 0; i < len(a); i++ {
+		switch a[i].SegmentCount() {
+		case 2:
+			known[1] = a[i]
+		case 3:
+			known[7] = a[i]
+		case 4:
+			known[4] = a[i]
+		case 7:
+			known[8] = a[i]
+		}
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i].SegmentCount() != 6 {
+			continue
+		}
+		if a[i]&known[1] != known[1] {
+			known[6] = a[i]
+		} else if a[i]&known[4] == known[4] {
+			known[9] = a[i]
+		} else {
+			known[0] = a[i]
+		}
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i].SegmentCount() != 5 {
+			continue
+		}
+		if a[i]&known[1] == known[1] {
+			known[3] = a[i]
+		} else if a[i]&known[9] == a[i] {
+			known[5] = a[i]
+		} else {
+			known[2] = a[i]
+		}
+	}
+	return known
+}
+
+func Decode(cipher [10]Display, input ...Display) (output int) {
+	m := make(map[Display]int, len(cipher))
+	for i := 0; i < len(cipher); i++ {
+		m[cipher[i]] = i
+	}
+	for _, c := range input {
+		output *= 10
+		output += m[c]
+	}
+	return
+}
--- go/2021/day08/day08_test.go
+++ go/2021/day08/day08_test.go
@@ -1,2 +1,83 @@
 package day08
 
+import (
+	"aoc2021"
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+type fixture struct {
+	Patterns [10]Display
+	Output   [4]Display
+}
+
+func parseFixture(s string) (*fixture, error) {
+	c := &fixture{}
+	parts := strings.Split(s, " ")
+	if len(parts) != 15 {
+		return nil, fmt.Errorf("bad fixture: %s", s)
+	}
+	for i := 0; i < 10; i++ {
+		p, err := ParseDisplay([]byte(parts[i]))
+		if err != nil {
+			return nil, err
+		}
+		c.Patterns[i] = p
+	}
+	for i := 0; i < 4; i++ {
+		p, err := ParseDisplay([]byte(parts[11+i]))
+		if err != nil {
+			return nil, err
+		}
+		c.Output[i] = p
+	}
+	return c, nil
+}
+
+func mustOpenFixture(name string) []*fixture {
+	f, err := aoc2021.OpenFixture(name)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	a := make([]*fixture, 0, 64)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		s := scanner.Text()
+		v, err := parseFixture(s)
+		if err != nil {
+			panic(err)
+		}
+		a = append(a, v)
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return a
+}
+
+func TestPart1(t *testing.T) {
+	count := 0
+	tests := mustOpenFixture("day08")
+	for _, test := range tests {
+		for _, output := range test.Output {
+			switch output.SegmentCount() {
+			case 2, 4, 3, 7: // segment counts of 1, 4, 7, 8
+				count++
+			}
+		}
+	}
+	aoc2021.AssertInt(t, 284, count, "bad value count")
+}
+
+func TestPart2(t *testing.T) {
+	sum := 0
+	tests := mustOpenFixture("day08")
+	for _, test := range tests {
+		cipher := Rewire(test.Patterns)
+		sum += Decode(cipher, test.Output[:]...)
+	}
+	aoc2021.AssertInt(t, 973499, sum, "bad display sum")
+}
