2d3a32b1d35d502d65fd6070a6e9792d0ebba84c
Ryan Armstrong <ryan@cavaliercoder.com>
2021-12-17 14:33:55 +0800

    2021 Day 10

--- go/2021/day10/day10.go
+++ go/2021/day10/day10.go
@@ -1,2 +1,61 @@
 package day10
 
+var chunks = map[byte]byte{
+	'(': ')',
+	'[': ']',
+	'{': '}',
+	'<': '>',
+}
+
+var errorValues = map[byte]int{
+	')': 3,
+	']': 57,
+	'}': 1197,
+	'>': 25137,
+}
+
+var autocompleteValues = map[byte]int{
+	')': 1,
+	']': 2,
+	'}': 3,
+	'>': 4,
+}
+
+func CheckSyntax(b []byte) (score int) {
+	stack := make([]byte, 0, len(b))
+	for _, c := range b {
+		if closer, ok := chunks[c]; ok {
+			stack = append(stack, closer)
+			continue
+		}
+		if len(stack) > 0 && stack[len(stack)-1] == c {
+			stack = stack[:len(stack)-1]
+			continue
+		}
+		score = errorValues[c]
+		break
+	}
+	return
+}
+
+func Autocomplete(b []byte) (score int) {
+	stack := make([]byte, 0, len(b))
+	for _, c := range b {
+		if closer, ok := chunks[c]; ok {
+			stack = append(stack, closer)
+			continue
+		}
+		if len(stack) > 0 && stack[len(stack)-1] == c {
+			stack = stack[:len(stack)-1]
+			continue
+		}
+		return 0
+	}
+	for len(stack) > 0 {
+		c := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		score *= 5
+		score += autocompleteValues[c]
+	}
+	return score
+}
--- go/2021/day10/day10_test.go
+++ go/2021/day10/day10_test.go
@@ -1,2 +1,46 @@
 package day10
 
+import (
+	"bufio"
+	"sort"
+	"testing"
+
+	. "aoc2021"
+)
+
+func mustOpenFixture(name string) [][]byte {
+	f := MustOpenFixture(name)
+	defer f.Close()
+	a := make([][]byte, 0, 64)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		b := make([]byte, len(scanner.Bytes()))
+		copy(b, scanner.Bytes())
+		a = append(a, b)
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return a
+}
+
+func TestPart1(t *testing.T) {
+	score := 0
+	a := mustOpenFixture("day10")
+	for _, b := range a {
+		score += CheckSyntax(b)
+	}
+	AssertInt(t, 294195, score, "bad syntax score")
+}
+
+func TestPart2(t *testing.T) {
+	a := mustOpenFixture("day10")
+	scores := make([]int, 0, len(a))
+	for _, b := range a {
+		if score := Autocomplete(b); score > 0 {
+			scores = append(scores, score)
+		}
+	}
+	sort.Ints(scores)
+	AssertInt(t, 3490802734, scores[len(scores)/2], "bad autocomplete score")
+}
