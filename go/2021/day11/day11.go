4fe46b5fd7b51fa933e27d35190f593fdd5f5272
Ryan Armstrong <ryan@cavaliercoder.com>
2021-12-17 15:47:06 +0800

    2021 Day 11

--- go/2021/day11/day11.go
+++ go/2021/day11/day11.go
@@ -1,2 +1,56 @@
 package day11
 
+import . "aoc2021"
+
+func Step(g *Grid) (flashes int) {
+	stack := make([]int, 0, 64)
+	for i, c := range g.Data {
+		if c == 9 {
+			stack = append(stack, i)
+			continue
+		}
+		if c > 9 {
+			panic("grid corrupted")
+		}
+		g.Data[i] = c + 1
+	}
+	for len(stack) > 0 {
+		i := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if g.Data[i] == 0 {
+			continue
+		}
+		flashes++
+		g.Data[i] = 0
+		for _, j := range g.Indexes(g.Adj(g.Pos(i))...) {
+			switch g.Data[j] {
+			case 0:
+				continue
+			case 9:
+				stack = append(stack, j)
+				continue
+			default:
+				g.Data[j]++
+			}
+		}
+	}
+	return
+}
+
+func GetSyncSteps(g *Grid) (steps int) {
+	isSynced := func(g *Grid) bool {
+		for _, c := range g.Data {
+			if c != 0 {
+				return false
+			}
+		}
+		return true
+	}
+	for {
+		if isSynced(g) {
+			return
+		}
+		steps++
+		Step(g)
+	}
+}
--- go/2021/day11/day11_test.go
+++ go/2021/day11/day11_test.go
@@ -1,2 +1,35 @@
 package day11
 
+import (
+	"testing"
+
+	. "aoc2021"
+)
+
+func mustOpenFixture(name string) *Grid {
+	f := MustOpenFixture(name)
+	defer f.Close()
+	g, err := ReadGrid(f)
+	if err != nil {
+		panic(err)
+	}
+	for i, c := range g.Data {
+		g.Data[i] = c - '0'
+	}
+	return g
+}
+
+func TestPart1(t *testing.T) {
+	flashes := 0
+	g := mustOpenFixture("day11")
+	for i := 0; i < 100; i++ {
+		flashes += Step(g)
+	}
+	AssertInt(t, 1755, flashes, "bad flash count")
+}
+
+func TestPart2(t *testing.T) {
+	g := mustOpenFixture("day11")
+	steps := GetSyncSteps(g)
+	AssertInt(t, 212, steps, "bad step count")
+}
