b00a94541279316c24f5783da8f43006d055f923
Ryan Armstrong <ryan@cavaliercoder.com>
2021-12-18 20:25:09 +0800

    2021 Day 14

--- go/2021/day14/day14.go
+++ go/2021/day14/day14.go
@@ -1,2 +1,58 @@
 package day14
 
+import "fmt"
+
+type Pair [2]byte
+
+type Template struct {
+	countByPair    map[Pair]int
+	countByPolymer map[byte]int
+}
+
+func NewTemplate(elem ...byte) *Template {
+	tmpl := &Template{
+		countByPair:    make(map[Pair]int, 256),
+		countByPolymer: make(map[byte]int, 256),
+	}
+	if len(elem) == 0 {
+		return tmpl
+	}
+	for i := 0; i < len(elem); i++ {
+		if i < len(elem)-1 {
+			k := Pair{elem[i], elem[i+1]}
+			tmpl.countByPair[k]++
+		}
+		tmpl.countByPolymer[elem[i]]++
+	}
+	return tmpl
+}
+
+func (c *Template) Step(rules map[Pair]byte) {
+	newPairs := make(map[Pair]int, len(c.countByPair))
+	for k, n := range c.countByPair {
+		b, ok := rules[k]
+		if !ok {
+			panic(fmt.Sprintf("bad pair: %s", k))
+		}
+		c.countByPolymer[b] += n
+		c.countByPair[k] -= n
+		newPairs[Pair{k[0], b}] += n
+		newPairs[Pair{b, k[1]}] += n
+	}
+	for k, v := range newPairs {
+		c.countByPair[k] += v
+	}
+}
+
+func (c *Template) Hash() int {
+	min, max := -1, -1
+	for _, v := range c.countByPolymer {
+		if min < 0 || v < min {
+			min = v
+		}
+		if max < 0 || v > max {
+			max = v
+		}
+	}
+	return max - min
+}
--- go/2021/day14/day14_test.go
+++ go/2021/day14/day14_test.go
@@ -1,2 +1,60 @@
 package day14
 
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"testing"
+
+	. "aoc2021"
+)
+
+func mustReadFixture(r io.Reader) (tmpl *Template, rules map[Pair]byte) {
+	scanner := bufio.NewScanner(r)
+	if scanner.Scan() {
+		tmpl = NewTemplate(scanner.Bytes()...)
+	}
+	rules = make(map[Pair]byte)
+	scanner.Scan()
+	for scanner.Scan() {
+		b := scanner.Bytes()
+		a := bytes.Split(b, []byte(" -> "))
+		if len(a) != 2 {
+			panic(fmt.Sprintf("bad pair insertion: %s", b))
+		}
+		if len(a[0]) != 2 {
+			panic(fmt.Sprintf("bad pair insertion: %s", b))
+		}
+		if len(a[1]) != 1 {
+			panic(fmt.Sprintf("bad pair insertion: %s", b))
+		}
+		rules[[2]byte{a[0][0], a[0][1]}] = a[1][0]
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return
+}
+
+func mustOpenFixture(name string) (tmpl *Template, rules map[Pair]byte) {
+	f := MustOpenFixture(name)
+	defer f.Close()
+	return mustReadFixture(f)
+}
+
+func TestPart1(t *testing.T) {
+	tmpl, rules := mustOpenFixture("day14")
+	for i := 0; i < 10; i++ {
+		tmpl.Step(rules)
+	}
+	AssertInt(t, 2170, tmpl.Hash(), "bad polymer hash")
+}
+
+func TestPart2(t *testing.T) {
+	tmpl, rules := mustOpenFixture("day14")
+	for i := 0; i < 40; i++ {
+		tmpl.Step(rules)
+	}
+	AssertInt(t, 2422444761283, tmpl.Hash(), "bad polymer hash")
+}
