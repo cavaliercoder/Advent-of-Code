796f94ab876747cd70e03a25e3da33cd065cbb75
Ryan Armstrong <ryan@cavaliercoder.com>
2021-12-21 12:03:16 +0800

    2021 Day 18

--- go/2021/day18/day18.go
+++ go/2021/day18/day18.go
@@ -1,2 +1,210 @@
 package day18
 
+import (
+	"bytes"
+	"fmt"
+	"io"
+)
+
+// Number is a snailfish number.
+type Number struct {
+	N    int
+	L, R *Number
+}
+
+func (n *Number) Copy() *Number {
+	if n == nil {
+		return nil
+	}
+	return &Number{
+		N: n.N,
+		L: n.L.Copy(),
+		R: n.R.Copy(),
+	}
+}
+
+func (n *Number) IsRegular() bool {
+	return n.L == nil && n.R == nil
+}
+
+// M returns the magnitude of the snailfish number.
+func (n *Number) M() int {
+	if n.IsRegular() {
+		return n.N
+	}
+	return 3*n.L.M() + 2*n.R.M()
+}
+
+func (n *Number) reduce() {
+	for {
+		if n.explode(0, nil, nil) {
+			continue
+		}
+		if n.split() {
+			continue
+		}
+		return
+	}
+}
+
+func (n *Number) explode(depth int, l, r *Number) (ok bool) {
+	if n.IsRegular() {
+		return false
+	}
+	if depth == 4 {
+		if !n.L.IsRegular() || !n.R.IsRegular() {
+			panic("why?")
+		}
+		if l != nil {
+			for !l.IsRegular() {
+				l = l.R
+			}
+			l.N += n.L.N
+		}
+		if r != nil {
+			for !r.IsRegular() {
+				r = r.L
+			}
+			r.N += n.R.N
+		}
+		n.N, n.L, n.R = 0, nil, nil
+		return true
+	}
+	if ok := n.L.explode(depth+1, l, n.R); ok {
+		return true
+	}
+	if ok := n.R.explode(depth+1, n.L, r); ok {
+		return true
+	}
+	return false
+}
+
+func (n *Number) split() (ok bool) {
+	if n.IsRegular() {
+		if n.N > 9 {
+			n.L = &Number{
+				N: n.N / 2,
+			}
+			n.R = &Number{
+				N: n.N/2 + n.N%2,
+			}
+			return true
+		}
+		return false
+	}
+	if ok := n.L.split(); ok {
+		return true
+	}
+	if ok := n.R.split(); ok {
+		return true
+	}
+	return false
+}
+
+func (n *Number) Format(w io.Writer) {
+	if n.IsRegular() {
+		fmt.Fprintf(w, "%d", n.N)
+	} else {
+		fmt.Fprint(w, "[")
+		n.L.Format(w)
+		fmt.Fprint(w, ",")
+		n.R.Format(w)
+		fmt.Fprint(w, "]")
+	}
+}
+
+func (n *Number) String() string {
+	b := new(bytes.Buffer)
+	n.Format(b)
+	return b.String()
+}
+
+// Add returns a new number that is the sum of multiple snailfish numbers.
+func Add(A ...*Number) *Number {
+	if len(A) == 0 {
+		return nil
+	}
+	a := A[0].Copy()
+	for _, b := range A[1:] {
+		a = &Number{L: a, R: b.Copy()}
+		a.reduce()
+	}
+	return a
+}
+
+// MaxMagnitude returns the maximum magnitude for the sum of any two pairs of
+// snailfish numbers in O(n²).
+func MaxMagnitude(A ...*Number) int {
+	max := 0
+	for _, a := range A {
+		for _, b := range A {
+			if a == b {
+				continue
+			}
+			m := Add(a, b).M()
+			if m > max {
+				max = m
+			}
+		}
+	}
+	return max
+}
+
+func Parse(b []byte) *Number {
+	c := &parser{
+		data: b,
+	}
+	return c.Parse()
+}
+
+type parser struct {
+	data []byte
+	ptr  int
+}
+
+func (c *parser) Next() {
+	if !c.EOF() {
+		c.ptr++
+	}
+}
+
+func (c *parser) Expect(b byte) {
+	if c.B() != b {
+		panic(fmt.Sprintf("expected: '%c', got '%c' at %d", b, c.B(), c.ptr))
+	}
+	c.Next()
+}
+
+func (c *parser) EOF() bool { return c.ptr >= len(c.data) }
+
+func (c *parser) B() byte {
+	if c.EOF() {
+		return 0
+	}
+	return c.data[c.ptr]
+}
+
+func (c *parser) IsNumeric() bool {
+	if c.EOF() {
+		return false
+	}
+	return c.B() >= '0' && c.B() <= '9'
+}
+
+func (c *parser) Parse() *Number {
+	n := &Number{}
+	if c.IsNumeric() {
+		for c.IsNumeric() {
+			n.N *= 10
+			n.N += int(c.data[c.ptr] - '0')
+			c.Next()
+		}
+		return n
+	}
+	c.Expect('[')
+	n.L = c.Parse()
+	c.Expect(',')
+	n.R = c.Parse()
+	c.Expect(']')
+	return n
+}
--- go/2021/day18/day18_test.go
+++ go/2021/day18/day18_test.go
@@ -1,2 +1,110 @@
 package day18
 
+import (
+	"bufio"
+	"fmt"
+	"testing"
+
+	. "aoc2021"
+)
+
+func mustOpenFixture(name string) []*Number {
+	f := MustOpenFixture(name)
+	defer f.Close()
+	a := make([]*Number, 0, 64)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		s := scanner.Bytes()
+		a = append(a, Parse(s))
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return a
+}
+
+func TestParser(t *testing.T) {
+	tests := []string{
+		"[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+		"[[[5,[2,8]],4],[5,[[9,9],0]]]",
+		"[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+		"[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+		"[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+		"[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+		"[[[[5,4],[7,7]],8],[[8,3],8]]",
+		"[[9,3],[[9,9],[6,[4,9]]]]",
+		"[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+		"[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]",
+	}
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("%02d", i+1), func(t *testing.T) {
+			n := Parse([]byte(test))
+			AssertString(t, test, n.String(), "bad parser output")
+		})
+	}
+}
+
+func TestExplode(t *testing.T) {
+	tests := map[string]string{
+		"[[[[[9,8],1],2],3],4]":                 "[[[[0,9],2],3],4]",
+		"[7,[6,[5,[4,[3,2]]]]]":                 "[7,[6,[5,[7,0]]]]",
+		"[[6,[5,[4,[3,2]]]],1]":                 "[[6,[5,[7,0]]],3]",
+		"[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]": "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]",
+		"[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]":     "[[3,[2,[8,0]]],[9,[5,[7,0]]]]",
+	}
+	for input, expect := range tests {
+		n := Parse([]byte(input))
+		n.explode(0, nil, nil)
+		AssertString(t, expect, n.String(), "bad reduction")
+	}
+}
+
+func TestSplit(t *testing.T) {
+	tests := map[string]string{
+		"[[[[0,7],4],[15,[0,13]]],[1,1]]":    "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]",
+		"[[[[0,7],4],[[7,8],[0,13]]],[1,1]]": "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]",
+	}
+	for input, expect := range tests {
+		n := Parse([]byte(input))
+		n.split()
+		AssertString(t, expect, n.String(), "bad split")
+	}
+}
+
+func TestAdd(t *testing.T) {
+	a := Parse([]byte("[[[[4,3],4],4],[7,[[8,4],9]]]"))
+	b := Parse([]byte("[1,1]"))
+	v := Add(a, b)
+	AssertString(
+		t,
+		"[[[[0,7],4],[[7,8],[6,0]]],[8,1]]",
+		v.String(),
+		"bad addition",
+	)
+}
+
+func TestMaxMagnitude(t *testing.T) {
+	A := []*Number{
+		Parse([]byte("[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]")),
+		Parse([]byte("[[[5,[2,8]],4],[5,[[9,9],0]]]")),
+		Parse([]byte("[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]")),
+		Parse([]byte("[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]")),
+		Parse([]byte("[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]")),
+		Parse([]byte("[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]")),
+		Parse([]byte("[[[[5,4],[7,7]],8],[[8,3],8]]")),
+		Parse([]byte("[[9,3],[[9,9],[6,[4,9]]]]")),
+		Parse([]byte("[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]")),
+		Parse([]byte("[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]")),
+	}
+	AssertInt(t, 3993, MaxMagnitude(A...), "bad maximum magnitude")
+}
+
+func TestPart1(t *testing.T) {
+	a := mustOpenFixture("day18")
+	AssertInt(t, 3524, Add(a...).M(), "bad magnitude")
+}
+
+func TestPart2(t *testing.T) {
+	a := mustOpenFixture("day18")
+	AssertInt(t, 4656, MaxMagnitude(a...), "bad maximum magnitude")
+}
