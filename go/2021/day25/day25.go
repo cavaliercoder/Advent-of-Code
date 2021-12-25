b17d1107f9c301f9de78534712c57fb04a084828
Ryan Armstrong <ryan@cavaliercoder.com>
2021-12-26 00:06:50 +0800

    2021 Day 25

--- go/2021/day25/day25.go
+++ go/2021/day25/day25.go
@@ -1,2 +1,66 @@
 package day25
 
+import (
+	. "aoc2021"
+)
+
+func stepEast(g *Grid) (*Grid, bool) {
+	moved := false
+	g2 := NewGrid(g.Width, g.Height).Reset('.')
+	for y := 0; y < g.Height; y++ {
+		for x := 0; x < g.Width; x++ {
+			p := Pos{X: x, Y: y}
+			g2.Set(p, g.MustGet(p))
+			if g.MustGet(p) != '>' {
+				continue
+			}
+			p2 := Pos{X: (x + 1) % g.Width, Y: y}
+			if g.MustGet(p2) == '.' {
+				g2.Set(p, '.')
+				g2.Set(p2, '>')
+				x++
+				moved = true
+			}
+		}
+	}
+	return g2, moved
+}
+
+func stepSouth(g *Grid) (*Grid, bool) {
+	moved := false
+	g2 := NewGrid(g.Width, g.Height).Reset('.')
+	for x := 0; x < g.Width; x++ {
+		for y := 0; y < g.Height; y++ {
+			p := Pos{X: x, Y: y}
+			g2.Set(p, g.MustGet(p))
+			if g.MustGet(p) != 'v' {
+				continue
+			}
+			p2 := Pos{X: x, Y: (y + 1) % g.Height}
+			if g.MustGet(p2) == '.' {
+				g2.Set(p, '.')
+				g2.Set(p2, 'v')
+				y++
+				moved = true
+			}
+		}
+	}
+	return g2, moved
+}
+
+func step(g *Grid) (*Grid, bool) {
+	var movedEast, movedSouth bool
+	g, movedEast = stepEast(g)
+	g, movedSouth = stepSouth(g)
+	return g, movedEast || movedSouth
+}
+
+func MaxSteps(g *Grid) int {
+	var moved bool
+	for i := 1; ; i++ {
+		g, moved = step(g)
+		if !moved {
+			return i
+		}
+	}
+}
--- go/2021/day25/day25_test.go
+++ go/2021/day25/day25_test.go
@@ -1,2 +1,22 @@
 package day25
 
+import (
+	"testing"
+
+	. "aoc2021"
+)
+
+func mustOpenFixture(name string) *Grid {
+	f := MustOpenFixture(name)
+	defer f.Close()
+	g, err := ReadGrid(f)
+	if err != nil {
+		panic(err)
+	}
+	return g
+}
+
+func TestPart1(t *testing.T) {
+	g := mustOpenFixture("day25")
+	AssertInt(t, 305, MaxSteps(g), "bad step count")
+}
