72cb0f32e80fff24878e3b776b4aedc11dc660dd
Ryan Armstrong <ryan@cavaliercoder.com>
2023-01-16 18:21:35 -0500

    AoC 2022 Day 20: Improved pseed using contiguous memory

--- cpp/src/2022/day20.cc
+++ cpp/src/2022/day20.cc
@@ -6,26 +6,11 @@
 namespace aoc2022 {
 
 class Day20 {
-  std::vector<int64_t> parse(aoc::Input& in) {
-    std::vector<int64_t> a;
-    int64_t n;
-    while (in) {
-      in.get_int(n).expect('\n');
-      a.push_back(n);
-    }
-    return a;
-  }
-
   struct Node {
-    const int64_t id = 0;
-    const int64_t value = 0;
-
+    int64_t id = 0;
+    int64_t value = 0;
     Node* prev = nullptr;
     Node* next = nullptr;
-
-    Node(const int64_t id, const int64_t value) : id(id), value(value) {}
-
-    ~Node() {}  // TODO
 
     void remove() {
       prev->next = next;
@@ -54,66 +39,61 @@ class Day20 {
     }
   };
 
-  std::unordered_map<int64_t, Node*> make_nodes(const std::vector<int64_t>& a,
-                                                const int64_t key = 1) {
-    std::unordered_map<int64_t, Node*> nodes;
-    auto first = new Node(0, a[0] * key);
-    nodes[0] = first;
-    auto prev = first;
-    for (int i = 1; i < a.size(); ++i) {
-      auto node = new Node(i, a[i] * key);
-      nodes[i] = node;
-      prev->next = node;
-      node->prev = prev;
-      prev = node;
+  std::vector<Node> make_nodes(const int64_t key = 1) const {
+    auto node =
+        std::vector<Node>(numbers_.size(), Node());  // needs point stability
+    for (int i = 0; i < numbers_.size(); ++i) {
+      node[i].id = i;
+      node[i].value = numbers_[i] * key;
+      node[i].prev = &node[(i - 1 + numbers_.size()) % numbers_.size()];
+      node[i].next = &node[(i + 1 + numbers_.size()) % numbers_.size()];
     }
-    prev->next = first;
-    first->prev = prev;
-    return nodes;
+    return node;
   }
 
-  int64_t mix(std::vector<int64_t>& a, const int rounds = 1,
-              const int64_t key = 1) {
-    int64_t len = a.size() - 1;
-    auto nodes = make_nodes(a, key);
+  int64_t mix(const int rounds = 1, const int64_t key = 1) const {
+    int64_t len = numbers_.size() - 1;
+    auto node = make_nodes(key);
     Node* zero = nullptr;
     for (int r = 0; r < rounds; ++r) {
-      for (int64_t id = 0; id < a.size(); ++id) {
-        auto n = nodes[id];
-        if (n->value == 0) zero = n;
-        auto after = n->prev;
-        n->remove();
-        after = after->nextn(n->value % len);
-        n->insert(after);
+      for (int64_t id = 0; id < numbers_.size(); ++id) {
+        auto& n = node[id];
+        if (n.value == 0) zero = &n;
+        auto after = n.prev;
+        n.remove();
+        after = after->nextn(n.value % len);
+        n.insert(after);
       }
     }
-
     uint64_t sum = 0;
-    zero = zero->nextn(1000 % a.size());
+    zero = zero->nextn(1000 % numbers_.size());
     sum += zero->value;
-    zero = zero->nextn(1000 % a.size());
+    zero = zero->nextn(1000 % numbers_.size());
     sum += zero->value;
-    zero = zero->nextn(1000 % a.size());
+    zero = zero->nextn(1000 % numbers_.size());
     sum += zero->value;
     return sum;
   }
 
+  std::vector<int64_t> numbers_;
+
  public:
-  int64_t Part1(aoc::Input in) {
-    auto a = parse(in);
-    return mix(a);
+  Day20(aoc::Input in) {
+    int64_t n;
+    while (in) {
+      in.get_int(n).expect('\n');
+      numbers_.push_back(n);
+    }
   }
 
-  int64_t Part2(aoc::Input in) {
-    auto a = parse(in);
-    return mix(a, 10, 811589153);
-  };
+  int64_t Part1() const { return mix(); }
+  int64_t Part2() const { return mix(10, 811589153); };
 };
 
-TEST(Day20, Part1) { EXPECT_EQ(Day20().Part1(aoc::Input(2022, 20)), 3466); }
+TEST(Day20, Part1) { EXPECT_EQ(Day20(aoc::Input(2022, 20)).Part1(), 3466); }
 
 TEST(Day20, Part2) {
-  EXPECT_EQ(Day20().Part2(aoc::Input(2022, 20)), 9995532008348);
+  EXPECT_EQ(Day20(aoc::Input(2022, 20)).Part2(), 9995532008348);
 }
 
 }  // namespace aoc2022
